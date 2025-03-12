2fef81f5922dd5bfe1cc82ffac3b33e7f003743a
Denis <dkashapov3@gmail.com>
2025-03-12 14:03:27 +0300

    tasks1

--- Practice1/task1.C
+++ Practice1/task1.C
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <locale.h>
 
-int main() {
+int main () {
     setlocale(LC_ALL, "Russian");
 
     int mass;
-    printf("Введите вашу массу в кг: ");
+    printf("Enter your massa: ");
     scanf("%d", &mass);
 
     if (mass >= 60 && mass <= 90) {
-        printf("Вы можете пройти отбор на Буран!\n");
+        printf("you can qualify!!!");
     } else {
-        printf("Извините, ваша масса не соответствует требованиям.\n");
+        printf("you not can qualify(");
     }
 
     return 0;
